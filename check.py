47a0f29d6f6f8d42ee298fcfbf65a93c0366b40a
Alireza <alireza97hi@gmail.com>
2017-05-27 22:47:11 +0430

    Parenthes and Brace Checker added

--- check.py
+++ check.py
@@ -1,7 +1,8 @@
-def commaCheck(file_input):
-    # It checks whether the number of open and close parentheses are equal or not
+def parenthes_brace_Check(file_input):
+    # It checks whether the number of open and close parentheses or braces are equal or not
     line_number = 1
     comma_counter = 0
+    brace_counter = 0
     tmp_token = ''
     for line in file_input:
         for token in line.replace('\t',' ').split(' '):
@@ -10,10 +11,18 @@ def commaCheck(file_input):
             elif token.strip() == ')':
                 comma_counter -= 1
                 if comma_counter<0:
-                    return False,str(line_number),str(tmp_token)
-            else:
-                tmp_token = token
+                    return False,str(line_number),str(tmp_token),'parenthese'
+            elif token.strip() == '{':
+                brace_counter += 1
+            elif token.strip() == '}':
+                brace_counter -= 1
+                if brace_counter <0:
+                    return False,str(line_number),str(tmp_token),'brace'
+            tmp_token = token
         line_number += 1
-    if comma_counter>0:
-        return False,'0','1'
-    return True,[],[]
+    if comma_counter > 0:
+        return False,'0','1','parenthes'
+    elif brace_counter > 0:
+        return False,'0','1','brace'
+    return True,[],[],[]
+
--- compiler.py
+++ compiler.py
@@ -1,11 +1,17 @@
 from check import *
 
 dir = open('test.txt','r')
-commabool,commaRes,commaToken = commaCheck(dir)
-if commabool:
-    print('yay')
+parenthes_brace_bool, parenthes_brace_Res, parenthes_brace_Token, problem_type = parenthes_brace_Check(dir)
+if parenthes_brace_bool:
+    print('Continue to Write')
 else:
-    if commaRes == '0':
-        print('Error : Comma hasn\'t been closed somewhere')
+    if parenthes_brace_Res == '0':
+        if problem_type == 'parenthes':
+            print('Error : Parenthes hasn\'t been closed somewhere')
+        else:
+            print('Error : Brace hasn\'t been closed somewhere')
     else:
-        print('Error : Comma Not Expected in line '+commaRes+' after '+commaToken)
+        if problem_type == 'parenthes':
+            print('Error : Parenthese Not Expected ' + parenthes_brace_Res + ' after \"' + parenthes_brace_Token +'\" - line ' + parenthes_brace_Res)
+        else:
+            print('Error : Brace Not Expected ' + parenthes_brace_Res + ' after \"' + parenthes_brace_Token +'\" - line ' + parenthes_brace_Res)
