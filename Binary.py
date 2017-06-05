2ff2a67f5a9138fa605de87ad8d0aa4e7d30c2af
Alireza <alireza97hi@gmail.com>
2017-06-05 11:57:53 +0430

    Binary to Decimal added

--- Binary.py
+++ Binary.py
@@ -5,3 +5,13 @@ def decimal_to_binary(dec_num):
         binary = str(number%2)+str(binary)
         number = int(number/2)
     return binary
+
+
+def binary_to_decimal(bin_num):
+    binary = str(bin_num)
+    power = len(bin_num) - 1
+    decimal = 0
+    for digit in binary:
+        decimal += (2**power) * int(digit)
+        power -= 1
+    return str(decimal)
