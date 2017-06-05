4ab7eddd20047f87678d96702f1ea86b593048a6
Alireza <alireza97hi@gmail.com>
2017-06-05 19:15:06 +0430

    Binary Function updated

--- Binary.py
+++ Binary.py
@@ -1,9 +1,16 @@
-def decimal_to_binary(dec_num):
-    number = abs(dec_num)
+def decimal_to_binary(dec_num='', bin_len=16,low=False,high=False):
+    number = abs(int(dec_num))
     binary = ''
     while number >= 1:
-        binary = str(number%2)+str(binary)
+        binary = str(number % 2)+str(binary)
         number = int(number/2)
+    len_digit = len(binary)
+    for i in range(bin_len-len_digit):
+        binary = '0'+binary
+    if low:
+        return binary[8:]
+    if high:
+        return binary[:8]
     return binary
 
 
@@ -15,3 +22,8 @@ def binary_to_decimal(bin_num):
         decimal += (2**power) * int(digit)
         power -= 1
     return str(decimal)
+
+def complement(bin_num,sec_num):
+    bin_num= str(bin_num)
+    bin_len = len(bin_num) - 1
+    return 2**bin_len-int(binary_to_decimal(bin_num))+int(sec_num)
